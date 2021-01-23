8fc7dbf05603eb7f0b29252d1082757dbc74322e
zach-brown <z.brown181818@protonmail.com>
2021-01-22 20:22:12 -0800

    First commit

---
+++ future_additions/api_key.py
@@ -0,0 +1,6 @@
+# Google Street View Static Images API
+google_api_key = 'AIzaSyDyIRm-Y64x-RthXNfwHa1z_FBuat5eweg'
+
+# arcgis API
+arcgis_username = 'OrangeTiger'
+arcgis_password = 'Dontf0rgetthestripes'
